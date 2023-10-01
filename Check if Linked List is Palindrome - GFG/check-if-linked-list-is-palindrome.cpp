de2984b8eebcaef49fcb95e50efa9eed5b62629b
Shibam2003 <129534182+Shibam2003@users.noreply.github.com>
2023-10-01 19:43:51 +0530

    Added solution - LeetHub

--- Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.cpp
+++ Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.cpp
@@ -28,34 +28,69 @@ struct Node {
   }
 };
 */
-#include<vector>
 
 class Solution{
     private:
-    bool checkPal(vector<int> arr){
-        int s = 0;
-        int e = arr.size() - 1;
-        while(s <= e){
-            if(arr[s] != arr[e]){
-                return 0;
-            }
-            s++;
-            e--;
+    
+     Node* getMid(Node* head ) {
+        Node* slow = head;
+        Node* fast = head -> next;
+        
+        while(fast != NULL && fast-> next != NULL) {
+            fast = fast -> next -> next;
+            slow = slow -> next;
         }
-        return 1;
+        
+        return slow;
+    }
+    Node* reverse(Node* head) {
+        
+        Node* curr = head;
+        Node* prev = NULL;
+        Node* next = NULL;
+        
+        while(curr != NULL) {
+            next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
     }
     
   public:
     //Function to check whether the list is palindrome.
     bool isPalindrome(Node *head)
     {
-        vector<int> arr;
-        Node* temp = head;
-        while(temp != NULL){
-            arr.push_back(temp -> data);
-            temp = temp -> next;
+         if(head -> next == NULL) {
+            return true;
         }
-        return checkPal(arr);
+        
+        //step 1 -> find Middle
+        Node* middle = getMid(head);
+        //cout << "Middle " << middle->data << endl;
+        
+        //step 2 -> reverse List after Middle
+        Node* temp = middle -> next;
+        middle -> next = reverse(temp);
+        
+        //step 3 -> Compare both halves
+        Node* head1 = head;
+        Node* head2 = middle -> next;
+        
+        while(head2 != NULL) {
+            if(head2->data != head1->data) {
+                return 0;
+            }
+            head1 = head1 -> next;
+            head2 = head2 -> next;
+        }
+    
+        //step 4 -> repeat step 2
+        temp = middle -> next;
+        middle -> next = reverse(temp);
+        
+        return true;
     }
 };
 
