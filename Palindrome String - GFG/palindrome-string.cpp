eb227d8c497146b5d5d2ac6c93ccdd7a0ecf2dd2
Shibam2003 <129534182+Shibam2003@users.noreply.github.com>
2023-10-09 21:32:29 +0530

    Added solution - LeetHub

---
+++ Palindrome String - GFG/palindrome-string.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string S)
+	{
+	    int len = S.length() - 1;
+	    int st = 0;
+	    while(st <= len){
+	        if(S[st] != S[len]){
+	            return 0;
+	        }
+	        st++;
+	        len--;
+	    }
+	    return 1;
+	    
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
