0aef2d65be2157ab6e43537335b0477e78c446d2
Shibam2003 <129534182+Shibam2003@users.noreply.github.com>
2023-10-14 23:58:06 +0530

    Added solution - LeetHub

--- Clone a linked list with next and random pointer - GFG/clone-a-linked-list-with-next-and-random-pointer.cpp
+++ Clone a linked list with next and random pointer - GFG/clone-a-linked-list-with-next-and-random-pointer.cpp
@@ -46,24 +46,41 @@ class Solution
             temp = temp -> next;
         }
         
-        //step 2: create a map 
-        unordered_map<Node* , Node*> oldToNewNode;
+        //step 2: clone node add in between original list
         Node* originalNode = head;
         Node* cloneNode = cloneHead;
         while(originalNode != NULL && cloneNode != NULL){
-            oldToNewNode[originalNode] = cloneNode;
-            originalNode = originalNode -> next;
-            cloneNode = cloneNode -> next;
+            Node* next = originalNode -> next;
+            originalNode -> next = cloneNode;
+            originalNode = next;
+            
+            next = cloneNode -> next;
+            cloneNode -> next = originalNode;
+            cloneNode = next;
         }
+        //step 3: random pointer copy
+        temp = head;
+        while(temp  != NULL){
+            if(temp -> next != NULL){
+                temp -> next -> arb = temp -> arb ? temp -> arb -> next : temp -> arb;
+                
+            }
+            temp = temp -> next -> next;
+        }
+        //step 4: revert changes done in step 2
         originalNode = head;
         cloneNode = cloneHead;
-        while(originalNode != NULL){
-            cloneNode -> arb = oldToNewNode[originalNode -> arb];
+        while(originalNode != NULL && cloneNode != NULL){
+            originalNode -> next = cloneNode -> next;
             originalNode = originalNode -> next;
+            if(originalNode != NULL){
+                cloneNode -> next = originalNode -> next;
+            }
             cloneNode = cloneNode -> next;
-            
         }
+        //step 5: return ans
         return cloneHead;
+        
     }
 
 };
